e2fb235a9d45d14e3d6cf9a56f33aab1194fce48
Kunihiko Ito <kuni-ito@esm.co.jp>
2012-07-09 13:12:14 +0900

    Rendering the first page

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -2,6 +2,10 @@ package controllers
 
 import play.api._
 import play.api.mvc._
+import models.Task
+import play.api.data._
+import play.api.data.Forms._
+
 
 object Application extends Controller {
 
@@ -9,10 +13,16 @@ object Application extends Controller {
     Redirect(routes.Application.tasks)
   }
 
-  def tasks = TODO
+  def tasks = Action {
+    Ok(views.html.index(Task.all(), taskForm))
+  }
 
   def newTask = TODO
 
   def deleteTask(id: Long) = TODO
+
+  val taskForm = Form(
+    "label" -> nonEmptyText
+  )
 
 }
