f340e4fd6c7ed76206eaaec2a6c9e1a6000194eb
Kunihiko Ito <kuni-ito@esm.co.jp>
2012-07-22 21:28:18 +0900

    Deleting tasks

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -27,7 +27,10 @@ object Application extends Controller {
     )
   }
 
-  def deleteTask(id: Long) = TODO
+  def deleteTask(id: Long) = Action {
+    Task.delete(id)
+    Redirect(routes.Application.tasks)
+  }
 
   val taskForm = Form(
     "label" -> nonEmptyText
